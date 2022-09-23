d4c632b3b990cbc83ad4af1b401e7a6fedfb9141
Fru1tJu1ce <83663507+Fru1tJu1ce@users.noreply.github.com>
2022-09-23 16:41:34 +0300

    Add files via upload

---
+++ insertion-sort/insertion_sort/insertion_sort/insertion_sort.cpp
@@ -0,0 +1,30 @@
+﻿#include "array_in_out.hpp"
+#include <iostream>
+using namespace std;
+
+
+int main(void)
+{
+    int i, j, key, counter=0, tmp;
+    vector<int> array = array_in_out();
+    for (j = 1; j < array.size(); j++)
+    {
+        key = array[j];
+        i = j - 1;
+        while ((i >= 0) && (array[i] > key)){
+            counter++;
+            tmp = array[i + 1];
+            array[i + 1] = array[i];
+            array[i] = tmp;
+            i = i - 1;
+        }
+        array[i + 1] = key;
+        py_output(array);
+    }
+    cout
+        << "\n Алгоритму потребовалось: "
+        << counter
+        << " сдвигов!\n";
+    system("Pause");
+    return 0;
+}
